6099bce5ae9058058a097dd6e71066ef8c3dba91
Michael <moaylesbury@gmail.com>
2021-03-19 11:15:03 +0000

    added part 3 files

---
+++ Receiver3.py
@@ -0,0 +1,9 @@
+
+class Receiver3:
+    def __init__(self):
+        super(Receiver3, self).__init__()
+
+
+
+if __name__ == "__main__":
+    receiver3 = Receiver3()
---
+++ Sender3.py
@@ -0,0 +1,8 @@
+
+class Sender3:
+    def __init__(self):
+        super(Sender3, self).__init__()
+
+
+if __name__ == "__main__":
+    sender3 = Sender3()
