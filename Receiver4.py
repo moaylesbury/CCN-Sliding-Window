4e6874d90b3b0a5beede81e4e2fb73992f255029
Michael <moaylesbury@gmail.com>
2021-03-20 13:49:54 +0000

    begin part 4 implementation

---
+++ Receiver4.py
@@ -0,0 +1,12 @@
+from Receiver3 import Receiver3
+
+class Receiver4(Receiver3):
+
+    def SelectiveRepeat(self):
+        pass
+
+
+
+if __name__ == "__main__":
+    receiver4 = Receiver4()
+    receiver4.SelectiveRepeat()
---
+++ Sender4.py
@@ -0,0 +1,11 @@
+from Sender3 import Sender3
+
+class Sender4(Sender3):
+
+    def SelectiveRepeat(self):
+        pass
+
+
+if __name__ == "__main__":
+    sender4 = Sender4()
+    sender4.SelectiveRepeat()
