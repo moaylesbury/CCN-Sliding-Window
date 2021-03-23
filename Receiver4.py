c3919f107dcbb94b63660391c657a171568111f5
Michael <moaylesbury@gmail.com>
2021-03-23 11:50:35 +0000

    about to make some changes

--- Receiver4.py
+++ Receiver4.py
@@ -24,6 +24,8 @@ class Receiver4(Receiver3):
         #######
         base = 0
 
+
+
         expected_seq_no = 0
         data = None
         self.EOF = (0).to_bytes(1, "big")
@@ -33,35 +35,84 @@ class Receiver4(Receiver3):
 
             img_bytes, seq_no = receiver4.Receive(server_socket)
             seq_no = int.from_bytes(seq_no, 'big')
-
             print("RECIEVED : : : ", seq_no)
-            if seq_no == expected_seq_no:
-                expected_seq_no += 1
-                print("Base increase as ", seq_no, " == ", expected_seq_no)
-                base += 1
-                data = receiver4.append_data(data, img_bytes)
-                receiver4.SendAck(server_socket, seq_no)
-                buffer = receiver4.shuffle_buffer(buffer)
 
-                shuffle_amount = 0
-                for b in range(1, self.window_size):
-                    if buffer[b] != 0:
-                        # print("base increase because")
+            # receive packet
+            # - if seq_num == expected_seq_num
+            # --extract data
+            # --base += 1
+            # --expected_seq_num += 1
+            # --shuffle buffer
+            # --loop through buffer
+            # --- if buffer != 0
+            # ---base += 1
+            # ---extract data
+            # ---shuffle buffer
+            # --else
+            # ---break
+            # -elsif seq_num between base and base + window size
+            # --add data to buffer
+            if seq_no == expected_seq_no:
+                print("--==--=-==-==--==-===-=-")
+                print("--==--=-==-==--==-===-=-")
+                print("--==--=-==-==--==-===-=-")
+                print("--==--=-==-==--==-===-=-")
+                data = receiver4.append_data(data, img_bytes)
+                base += 1
+                expected_seq_no += 1
+                receiver4.SendAck(server_socket, seq_no)
+                buffer = receiver4.shuffle_buffer(buffer)  # stops timer
+                for t in range(self.window_size):
+                    if buffer[t] != 0:
+                        print("over here! ", buffer)
                         base += 1
-                        shuffle_amount += 1
-                        expected_seq_no += 1
-                        data = receiver4.append_data(data, img_bytes)
+                        buffer = receiver4.shuffle_buffer(buffer)
                     else:
                         break
-                for i in range(shuffle_amount):
-                    buffer = receiver4.shuffle_buffer(buffer)
-
-
-            elif seq_no < base + self.window_size:
+            elif base <= seq_no < base + self.window_size:
                 buffer[seq_no % self.window_size] = img_bytes
                 receiver4.SendAck(server_socket, seq_no)
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+            # if seq_no == expected_seq_no:
+            #     expected_seq_no += 1
+            #     print("Base increase as ", seq_no, " == ", expected_seq_no)
+            #     base += 1
+            #     data = receiver4.append_data(data, img_bytes)
+            #     receiver4.SendAck(server_socket, seq_no)
+            #     buffer = receiver4.shuffle_buffer(buffer)
+            #
+            #     shuffle_amount = 0
+            #     for b in range(1, self.window_size):
+            #         if buffer[b] != 0:
+            #             # print("base increase because")
+            #             base += 1
+            #             shuffle_amount += 1
+            #             expected_seq_no += 1
+            #             data = receiver4.append_data(data, img_bytes)
+            #         else:
+            #             break
+            #     for i in range(shuffle_amount):
+            #         buffer = receiver4.shuffle_buffer(buffer)
+            #
+
+            # elif seq_no < base + self.window_size:
+            #     buffer[seq_no % self.window_size] = img_bytes
+            #     receiver4.SendAck(server_socket, seq_no)
+            #
+            #
 
 
 
--- Sender4.py
+++ Sender4.py
@@ -24,6 +24,10 @@ class Sender4(Sender3):
                 timers[i] = timers[i + 1]
         return timers
 
+    def retrans_seq_no(self, t, next_seq_no):
+        self.sequenceNumber = (next_seq_no - (self.window_size - t)).to_bytes(2, 'big')
+        # self.sequenceNumber = (next_seq_no + (t - (next_seq_no % self.window_size))).to_bytes(2, 'big')
+
     def SelectiveRepeat(self):
         client_socket = socket(AF_INET, SOCK_DGRAM)
         client_socket.setblocking(False)
@@ -33,64 +37,119 @@ class Sender4(Sender3):
         base = 0
         next_seq_no = 0
 
+        ## for testing
+        resent = []
+        acks_received = []
+        tpos = []
+        ##
+
         counter = 0
         timers = []
         for i in range(self.window_size):
             timers.append(0)
 
         while not eof:
+            print(timers)
             ack_seq_no = -1
             print("sequence number: ", next_seq_no)
             print("base number    : ", base)
-            if next_seq_no < base + self.window_size:   # TODO: remember to modulo
+            print("RESENT PACKETS: ", resent)
+            print("T POS: ", tpos)
+            print("ACKS RECEIVED: ", acks_received)
+
+            # sendpacket
+            # -if between base and base + window size
+            # --send packet
+            # --increase next_seq_num
+            # --start timer
+            if base <= next_seq_no < base + self.window_size:   # TODO: remember to modulo
                 self.sequenceNumber = next_seq_no.to_bytes(2, 'big')
                 sender4.send(client_socket, img_byte_arr[counter])
                 timers[next_seq_no % self.window_size] = time.time()
+                next_seq_no += 1
 
-
+            # check timers
+            # - if any timer has expired
+            # --resend packet with corresponding sequence number
+            # --start its timer
             for t in range(len(timers)):
                 if time.time() - timers[t] >= self.retry_timeout and timers[t] != 0:
                     print("+++++timeout+++++")
                     timers[t] = time.time()
-                    # next_seq_no = base
-
-                    # go
-                    if t == (next_seq_no % self.window_size):
-                        self.sequenceNumber = next_seq_no.to_bytes(2, "big")
-                    elif t < next_seq_no:
-                        self.sequenceNumber = (next_seq_no + ((next_seq_no % self.window_size)-t)).to_bytes(2, 'big')
-                    elif t > next_seq_no:
-                        self.sequenceNumber = (next_seq_no + (t-(next_seq_no % self.window_size))).to_bytes(2, 'big')
-                    print("RESENT: ", int.from_bytes(self.sequenceNumber, "big"))
+                    sender4.retrans_seq_no(t, next_seq_no) # changes sequence number
                     sender4.send(client_socket, img_byte_arr[counter - (len(timers) - i)])
+                    print("RESENT: ", int.from_bytes(self.sequenceNumber, "big"))
+                    resent.append(int.from_bytes(self.sequenceNumber, "big"))
+                    tpos.append(t)
 
-
+            # receive ack
+            # - if ack_seq_num == next_seq_num - 1
+            # --base += 1
+            # --shuffle timers
+            # --loop through timers
+            # --- if timer = 1
+            # ---base += 1
+            # ---shuffle timers
+            # -- else
+            # ---break
+            # -elif between base and base + window size
+            # --timer = 1
+            print("dsagjalsdkhkasnhklasklhnadh")
+            print("dsagjalsdkhkasnhklasklhnadh")
+            print("dsagjalsdkhkasnhklasklhnadh")
+            print("dsagjalsdkhkasnhklasklhnadh")
+            print("dsagjalsdkhkasnhklasklhnadh")
             try:
                 ack_pack, server_address = client_socket.recvfrom(4000)
                 ack_seq_no = ack_pack[0:2]
                 ack_seq_no = int.from_bytes(ack_seq_no, 'big')
+                print("=-=-=-=-=--=-=-=--=-=-=-=-=-=-=-=-=--=-=-=--=-=-=-=-=-=-=-=-=--=-=-=--=-=-=-=-")
+                print("=-=-=-=-=--=-=-=--=-=-=-=-=-=-=-=-=--=-=-=--=-=-=-=-=-=-=-=-=--=-=-=--=-=-=-=-")
+                print("=-=-=-=-=--=-=-=--=-=-=-=-=-=-=-=-=--=-=-=--=-=-=-=-=-=-=-=-=--=-=-=--=-=-=-=-")
+                print("ACK SQN: ", ack_seq_no, " NEXT SQN - 1: ", next_seq_no)
+                print("=-=-=-=-=--=-=-=--=-=-=-=-=-=-=-=-=--=-=-=--=-=-=-=-=-=-=-=-=--=-=-=--=-=-=-=-")
+                print("=-=-=-=-=--=-=-=--=-=-=-=-=-=-=-=-=--=-=-=--=-=-=-=-=-=-=-=-=--=-=-=--=-=-=-=-")
+                print("=-=-=-=-=--=-=-=--=-=-=-=-=-=-=-=-=--=-=-=--=-=-=-=-=-=-=-=-=--=-=-=--=-=-=-=-")
+                if ack_seq_no not in acks_received:
+                    acks_received.append(ack_seq_no)
+                if ack_seq_no == next_seq_no - 1:
+                    base += 1
+                    timers = sender4.shuffle_buffer(timers) # stops timer
+                    for t in range(self.window_size):
+                        if timers[t] == 1:
+                            print("over here! ", timers)
+                            base += 1
+                            timers = sender4.shuffle_buffer(timers)
+                        else:
+                            break
+                elif base <= ack_seq_no < base + self.window_size:
+                    timers[ack_seq_no % self.window_size] = 1
+
                 print("received ACK ", ack_seq_no)
-                timers[ack_seq_no % self.window_size] = 1
             except error:
                 pass
 
+            # check timers
+            # - if any timer has expired
+            # --resend packet with corresponding sequence number
+            # --start its timer
 
-
-            print("oi oi ! ", next_seq_no, " and recvd ", ack_seq_no)
-            if next_seq_no == ack_seq_no:
-                print("yes")
-                next_seq_no += 1
-                base += 1
-                shuffle_amount = 0
-                for t in range(self.window_size):
-                    if timers[t] == 1:
-                        base += 1
-                        shuffle_amount += 1
-                    else:
-                        break
-                for i in range(shuffle_amount):
-                    timers = sender4.shuffle_buffer(timers)
-
+            # print("oi oi ! ", next_seq_no, " and recvd ", ack_seq_no)
+            # if next_seq_no == ack_seq_no:
+            #     print("yes")
+            #     next_seq_no += 1
+            #     base += 1
+            #     shuffle_amount = 0
+            #     for t in range(self.window_size):
+            #         if timers[t] == 1:
+            #             base += 1
+            #             next_seq_no += 1
+            #             shuffle_amount += 1
+            #         else:
+            #             break
+            #     for i in range(shuffle_amount):
+            #         timers = sender4.shuffle_buffer(timers)
+            #
 
 
 
@@ -100,3 +159,4 @@ class Sender4(Sender3):
 if __name__ == "__main__":
     sender4 = Sender4()
     sender4.SelectiveRepeat()
+    # sender4.retrans_seq_no()
